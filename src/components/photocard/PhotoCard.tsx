37953848a0a3a3650dc52dc3d778688bf793f0ff
Arnaud Deprez <arnaudeprez@gmail.com>
2019-10-11 23:53:53 +0200

    fix: remove props usage in MDX file

--- src/components/photocard/PhotoCard.tsx
+++ src/components/photocard/PhotoCard.tsx
@@ -5,20 +5,24 @@ import pic from '../../../content/images/profile.png'
 import './PhotoCard.scss'
 
 export interface PhotoCardProps extends React.ComponentPropsWithoutRef<'div'> {
-  name: string
-  jobTitle: string
+  name?: string
+  jobTitle?: string
 }
 
 export const PhotoCard = ({ name, jobTitle, ...rest }: PhotoCardProps) => (
   <Card className="photo-card" {...rest}>
     <Card.Img src={pic} className="rounded-circle mb-2" />
     <Card.Body className="d-none d-lg-block">
-      <Card.Title className="text-center" as="h5">
-        {name}
-      </Card.Title>
-      <Card.Subtitle className="font-italic text-center text-wrap" as="h6">
-        {jobTitle}
-      </Card.Subtitle>
+      {name && (
+        <Card.Title className="text-center" as="h5">
+          {name}
+        </Card.Title>
+      )}
+      {jobTitle && (
+        <Card.Subtitle className="font-italic text-center text-wrap" as="h6">
+          {jobTitle}
+        </Card.Subtitle>
+      )}
     </Card.Body>
   </Card>
 )
--- src/components/photocard/__tests__/PhotoCard.spec.tsx
+++ src/components/photocard/__tests__/PhotoCard.spec.tsx
@@ -4,6 +4,10 @@ import { PhotoCard } from '../PhotoCard'
 
 describe('<PhotoCard/>', () => {
   it('renders correctly', () => {
+    const { asFragment } = render(<PhotoCard />)
+    expect(asFragment()).toMatchSnapshot()
+  })
+  it('renders correctly with name and jobTitle', () => {
     const { asFragment } = render(<PhotoCard name="test" jobTitle="job" />)
     expect(asFragment()).toMatchSnapshot()
   })
