e20f04059e78fa54ab49808131b6268469bf5857
Arnaud Deprez <arnaudeprez@gmail.com>
2019-10-30 16:29:53 +0100

    feat: try blue-steel/orange theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -190,7 +190,7 @@ module.exports = {
         lang: 'en',
         start_url: '/',
         background_color: '#fff',
-        theme_color: '#bd5d38',
+        theme_color: '#3B8EA5',
         display: 'minimal-ui',
         icon: './static/favicon/profile.png'
       }
