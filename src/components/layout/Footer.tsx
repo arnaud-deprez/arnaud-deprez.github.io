a7959c285268b47407abd46deac55af6aeeebdcb
Arnaud Deprez <arnaudeprez@gmail.com>
2019-11-21 22:04:33 +0100

    feat: add company legal info

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,22 +3,40 @@ import { Container, Col, Row, OverlayTrigger, Tooltip } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { OriginalIcon } from '../icon'
 
+import './Footer.scss'
+
 export const Footer = () => (
   <Container fluid as="footer">
     <hr />
-    <Row className="justify-content-around text-center">
-      <Col sm={12} lg={{ span: 3, offset: 2 }}>
-        <p>Copyright © 2019 Arnaud Deprez</p>
+    <Row className="justify-content-around">
+      <Col sm={12} lg={{ span: 3, offset: 2 }} className="footer-text">
+        <p>© 2019 Arnaud Deprez</p>
+        <address>
+          Owner of
+          <br />
+          <b>Powple SPRL</b>
+          <br />
+          Rue des Rivageois 7/21 - 4000 Liège
+          <br />
+          Belgium
+          <br />
+          Phone: <a href="tel:+3242531491">+32 4 253 14 91</a>
+          <br />
+          Email: <a href="mailto:info@powple.com">info@powple.com</a>
+          <br />
+          VAT: BE 0669.706.806
+        </address>
       </Col>
-      <Col sm={12} lg={3}>
+      <Col sm={12} lg={3} className="footer-text">
         <p>
           Build with{' '}
           <a href="https://www.gatsbyjs.org">
             <OverlayTrigger placement="top" overlay={<Tooltip id="footer-gatsby">Gatsby</Tooltip>}>
               <OriginalIcon icon="gatsby" inline />
             </OverlayTrigger>
-          </a>{' '}
-          | Designed with{' '}
+          </a>
+          {' | '}
+          Designed with{' '}
           <a href="https://getbootstrap.com">
             <OverlayTrigger
               placement="top"
@@ -26,8 +44,9 @@ export const Footer = () => (
             >
               <OriginalIcon icon="bootstrap" inline />
             </OverlayTrigger>
-          </a>{' '}
-          | Hosted with <FontAwesomeIcon icon="heart" color="red" /> by{' '}
+          </a>
+          {' | '}
+          Hosted with <FontAwesomeIcon icon="heart" color="red" /> by{' '}
           <a href="https://www.netlify.com/">
             <OverlayTrigger placement="top" overlay={<Tooltip id="footer-gatsby">Netlify</Tooltip>}>
               <OriginalIcon icon="netlify" inline />
