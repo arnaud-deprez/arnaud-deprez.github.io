ad52943d87e4c85d12a306cf30049c911e7f7f6f
Arnaud Deprez <arnaudeprez@gmail.com>
2019-11-30 12:04:48 +0100

    feat: add google analytics and google tag manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,6 +60,38 @@ module.exports = {
     }
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: 'UA-153625611-1',
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Any additional optional fields
+        siteSpeedSampleRate: 10,
+        // For GDPR <= 13 months, so 1 year is ok
+        cookieExpires: 31536000
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-google-tagmanager',
+      options: {
+        id: '6000219219',
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        // Defaults to null
+        defaultDataLayer: { platform: 'gatsby' }
+        // Specify optional GTM environment details.
+        /* gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        dataLayerName: "YOUR_DATA_LAYER_NAME", */
+      }
+    },
     'gatsby-plugin-typescript',
     'gatsby-plugin-sass',
     'gatsby-plugin-react-helmet',
