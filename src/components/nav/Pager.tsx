900978469cb630f307413d7d8b32059dfc64a991
Arnaud Deprez <arnaudeprez@gmail.com>
2020-04-13 17:40:38 +0200

    fix: pager link active color

--- src/components/nav/Pager.tsx
+++ src/components/nav/Pager.tsx
@@ -19,36 +19,19 @@ export const Pager = ({ prefix, page, total, maxVisiblePages = 5, className }: P
   firstVisiblePage = firstVisiblePage < 1 ? 1 : firstVisiblePage
   const visiblePages = total < maxVisiblePages ? total : maxVisiblePages
   const lastVisiblePage = firstVisiblePage + visiblePages - 1
-  // const show = total !== 1
-  const show = true
+  const show = total !== 1
   return (
     <Pagination className={className}>
-      {firstVisiblePage > 1 && (
-        <Pagination.First>
-          <Link to={pageUrl(prefix, 1)}></Link>
-        </Pagination.First>
-      )}
-      {page > 1 && (
-        <Pagination.Prev>
-          <Link to={pageUrl(prefix, page - 1)}></Link>
-        </Pagination.Prev>
-      )}
+      {firstVisiblePage > 1 && <Pagination.First as={Link} to={pageUrl(prefix, 1)} />}
+      {page > 1 && <Pagination.Prev as={Link} to={pageUrl(prefix, page - 1)} />}
       {show &&
         Array.from(Array(visiblePages), (x, i) => i + page).map((p) => (
-          <Pagination.Item key={p} active={page === p}>
-            <Link to={pageUrl(prefix, p)}>{p}</Link>
+          <Pagination.Item key={p} active={page === p} as={Link} to={pageUrl(prefix, p)}>
+            {p}
           </Pagination.Item>
         ))}
-      {page < total && (
-        <Pagination.Prev>
-          <Link to={pageUrl(prefix, page - 1)}></Link>
-        </Pagination.Prev>
-      )}
-      {lastVisiblePage < total && (
-        <Pagination.Last>
-          <Link to={pageUrl(prefix, total)}></Link>
-        </Pagination.Last>
-      )}
+      {page < total && <Pagination.Prev as={Link} to={pageUrl(prefix, page - 1)} />}
+      {lastVisiblePage < total && <Pagination.Last as={Link} to={pageUrl(prefix, total)} />}
     </Pagination>
   )
 }
