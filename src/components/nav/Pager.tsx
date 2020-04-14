b8cc4c374ed6f2e147c98768b128bbadcaeaa983
Arnaud Deprez <arnaudeprez@gmail.com>
2020-04-14 09:24:51 +0200

    fix: pager logic for showing page numbers

--- src/components/nav/Pager.tsx
+++ src/components/nav/Pager.tsx
@@ -2,8 +2,7 @@ import React from 'react'
 import { Pagination } from 'react-bootstrap'
 import { Link } from 'gatsby'
 
-const pageUrl = (prefix: string, page: number): string =>
-  page <= 1 ? `/${prefix}` : `/${prefix}/${page}`
+const pageUrl = (prefix: string, page: number): string => (page <= 1 ? prefix : `${prefix}/${page}`)
 
 export interface PagerProps {
   readonly prefix: string
@@ -14,24 +13,38 @@ export interface PagerProps {
 }
 
 export const Pager = ({ prefix, page, total, maxVisiblePages = 5, className }: PagerProps) => {
+  const normalizedPrefix = prefix.startsWith('/') ? prefix : `/${prefix}`
+  const lastFirstVisiblePage = total - maxVisiblePages + 1
   let firstVisiblePage = page - Math.floor((maxVisiblePages - 1) / 2)
   // if firstVisiblePage is < 1, then set it to 1
-  firstVisiblePage = firstVisiblePage < 1 ? 1 : firstVisiblePage
+  firstVisiblePage =
+    firstVisiblePage < 1
+      ? 1
+      : lastFirstVisiblePage < firstVisiblePage
+      ? lastFirstVisiblePage
+      : firstVisiblePage
   const visiblePages = total < maxVisiblePages ? total : maxVisiblePages
-  const lastVisiblePage = firstVisiblePage + visiblePages - 1
+  const lastVisiblePage = firstVisiblePage + visiblePages
   const show = total !== 1
   return (
     <Pagination className={className}>
-      {firstVisiblePage > 1 && <Pagination.First as={Link} to={pageUrl(prefix, 1)} />}
-      {page > 1 && <Pagination.Prev as={Link} to={pageUrl(prefix, page - 1)} />}
+      {firstVisiblePage > 1 && <Pagination.First as={Link} to={pageUrl(normalizedPrefix, 1)} />}
+      {page > 1 && <Pagination.Prev as={Link} to={pageUrl(normalizedPrefix, page - 1)} />}
       {show &&
-        Array.from(Array(visiblePages), (x, i) => i + page).map((p) => (
-          <Pagination.Item key={p} active={page === p} as={Link} to={pageUrl(prefix, p)}>
+        Array.from(Array(visiblePages), (x, i) => firstVisiblePage + i).map((p) => (
+          <Pagination.Item
+            key={p}
+            active={page === p}
+            as={page === p ? undefined : Link}
+            to={page === p ? undefined : pageUrl(normalizedPrefix, p)}
+          >
             {p}
           </Pagination.Item>
         ))}
-      {page < total && <Pagination.Prev as={Link} to={pageUrl(prefix, page - 1)} />}
-      {lastVisiblePage < total && <Pagination.Last as={Link} to={pageUrl(prefix, total)} />}
+      {page < total && <Pagination.Next as={Link} to={pageUrl(normalizedPrefix, page - 1)} />}
+      {lastVisiblePage < total && (
+        <Pagination.Last as={Link} to={pageUrl(normalizedPrefix, total)} />
+      )}
     </Pagination>
   )
 }
---
+++ src/components/nav/__tests__/Pager.spec.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { Pager } from '..'
+
+describe('<Pager />', () => {
+  it('should not render if there is only one page', () => {
+    const { asFragment } = render(<Pager prefix="/test" page={1} total={1} />)
+    expect(asFragment()).toMatchSnapshot()
+  })
+  it('should not render previous and first button on page 1', () => {
+    const { asFragment } = render(<Pager prefix="/test" page={1} total={10} />)
+    expect(asFragment()).toMatchSnapshot()
+  })
+  it('should not render next and last button on last page', () => {
+    const { asFragment } = render(<Pager prefix="/test" page={10} total={10} />)
+    expect(asFragment()).toMatchSnapshot()
+  })
+  it('should render 5 page items with first, previous, next and last button', () => {
+    const { asFragment } = render(<Pager prefix="/test" page={5} total={10} />)
+    expect(asFragment()).toMatchSnapshot()
+  })
+})
