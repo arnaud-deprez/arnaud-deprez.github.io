bf946cad580d67b55d891ad97f38c6c1aba9b7e5
Arnaud Deprez <arnaudeprez@gmail.com>
2020-11-15 21:32:36 +0100

    feat: remove intersection-observer polyfill

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,3 @@
 import { loadIcons } from './src/components/icon/FontAwesome'
 
 loadIcons()
-
-export const onClientEntry = async () => {
-  // eventually load polyfill for intersection-observer
-  if (typeof IntersectionObserver === `undefined`) {
-    await import(`intersection-observer`)
-  }
-}
